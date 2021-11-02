1b1841d8fd94374e21dbbd08950772b70236c291
Boris Nenchovski <bobinen@gmail.com>
2021-11-02 18:27:05 +0200

    Removed forgotten console log

--- ide-problems/view/problems.js
+++ ide-problems/view/problems.js
@@ -120,7 +120,6 @@ angular.module('problems', [])
         }
 
         $scope.showInfo = function(problem){
-            console.log("Clicked+ " + $scope.openedProblem);
             $scope.openedProblem = problem;
         }
 
